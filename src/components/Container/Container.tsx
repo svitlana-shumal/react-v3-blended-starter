5153d72d69007031989710ef68505806733651e6
Svitlana Shumal <svetlankachajka@gmail.com>
2025-08-06 13:23:10 +0300

    added api-key

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { ReactNode } from "react";
 
 import styled from "./Container.module.css";
+interface ContainerProp {
+  children: ReactNode;
+}
 
-export default function Container({ children }) {
+export default function Container({ children }: ContainerProp) {
   return <div className={styled.container}>{children}</div>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-v3-blended-starter/",
   build: {
     sourcemap: true,
   },
